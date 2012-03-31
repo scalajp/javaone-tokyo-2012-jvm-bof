ffab82cb39d0ac2c80ea812f74f552774121d32c
gakuzzzz <gaku_github@t2v.jp>
2012-03-31 19:56:53 +0900

    内部DSLが複数定義できなかった問題を修正

--- src/main/scala/org/scala_users/jp/dsl/internal.scala
+++ src/main/scala/org/scala_users/jp/dsl/internal.scala
@@ -4,19 +4,31 @@ import scala.util.DynamicVariable
 
 object internal extends ToProperties {
 
-  private[this] var values: List[(String, String)] = Nil
-
   private[this] val current = new DynamicVariable[String]("")
 
+  private[this] val values = new DynamicVariable[Option[Map[String, String]]](None)
+
   class Builder(key: String) {
-    def := (value: String) {
-      values = (concatKey(key), value) :: values
+
+    def := (value: String): Map[String, String] = {
+      val currentValues = values.value.getOrElse(Map())
+      val newValues = currentValues + (concatKey(key) -> value)
+      values.value = Some(newValues)
+      newValues
     }
 
     def := (body: => Any): Map[String, String] = {
       current.withValue(concatKey(key)) {
-        body
-        values.toMap
+        values.value match {
+          case Some(v) => {
+            body
+            v
+          }
+          case None => values.withValue(Some(Map())) {
+            body
+            values.value.getOrElse(Map())
+          }
+        }
       }
     }
     
