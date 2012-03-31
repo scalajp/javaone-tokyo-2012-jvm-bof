697f8768e36851f537efff22253c27ff7402b8c4
gakuzzzz <gaku_github@t2v.jp>
2012-04-01 01:26:44 +0900

    propertiesファイルへのシリアライズ機能追加

--- src/main/scala/org/scala_users/jp/dsl/ToProperties.scala
+++ src/main/scala/org/scala_users/jp/dsl/ToProperties.scala
@@ -1,13 +1,20 @@
 package org.scala_users.jp.dsl
 
 import java.util.Properties
+import java.io.OutputStream
 
 trait ToProperties {
 
   implicit def mapToProperties(map: Map[String, String]) = new {
+
     def toProperties: Properties = map.foldLeft(new Properties) {
       case (p, (key, value)) => p.setProperty(key, value); p
     }
+
+    def store(out: OutputStream, comments: String = null) {
+      toProperties.store(out, comments)
+    }
+
   }
 
 }
