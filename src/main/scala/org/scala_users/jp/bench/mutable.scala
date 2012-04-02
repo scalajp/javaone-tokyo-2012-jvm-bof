f3db498511205790471b305cba2105a4d89cff35
gakuzzzz <gaku_github@t2v.jp>
2012-04-03 00:08:48 +0900

    定義順をimmutableに揃え。適当につけたメソッド名をリネーム

--- src/main/scala/org/scala_users/jp/bench/mutable.scala
+++ src/main/scala/org/scala_users/jp/bench/mutable.scala
@@ -11,51 +11,13 @@ sealed abstract class RBTree {
 
   def get(key: String): String
   def put(key: String, value: String): RBTree = {
-    val result = p(key, value)
+    val result = insert(key, value)
     result.color = Black
     result
   }
-  def p(key: String, value: String): RBTree
+  private[bench] def insert(key: String, value: String): RBTree
   def height: Int
 }
-class Node(var color: Int, var key: String, var value: String, var left: RBTree, var right: RBTree) extends RBTree {
-  import RBTree._
-
-  def get(k: String) = {
-    val c = k compare key
-    if      (c < 0) left.get(k)
-    else if (c > 0) right.get(k)
-    else            value
-  }
-
-  def p(k: String, v: String) = {
-    val c = k compare key
-    if      (c < 0) left = left.p(k, v)
-    else if (c > 0) right = right.p(k, v)
-    else            value = v
-
-    var node: RBTree = this
-    if (node.left.color == Black && node.right.color == Red) node = rotateLeft(node)
-    if (node.left.color == Red && node.left.left.color == Red) node = rotateRight(node)
-    if (node.left.color == Red && node.right.color == Red) flipColors(node)
-    node
-  }
-
-  def height = left.height + color
-}
-
-object Empty extends RBTree {
-  import RBTree._
-
-  var color = Black
-  var left: RBTree = null
-  var right: RBTree = null
-
-  def get(key: String): String = null
-  def p(key: String, value: String) = new Node(Red, key, value, this, this)
-  val height = 0
-}
-
 object RBTree {
   val Black = 1
   val Red = 0
@@ -84,6 +46,42 @@ object RBTree {
     node.right.color = node.right.color ^ 1
   }
 
+}
+object Empty extends RBTree {
+  import RBTree._
+
+  var color = Black
+  var left: RBTree = null
+  var right: RBTree = null
+
+  def get(key: String): String = null
+  def insert(key: String, value: String) = new Node(Red, key, value, this, this)
+  val height = 0
+}
+class Node(var color: Int, var key: String, var value: String, var left: RBTree, var right: RBTree) extends RBTree {
+  import RBTree._
+
+  def get(k: String) = {
+    val c = k compare key
+    if      (c < 0) left.get(k)
+    else if (c > 0) right.get(k)
+    else            value
+  }
+
+  def insert(k: String, v: String) = {
+    val c = k compare key
+    if      (c < 0) left = left.insert(k, v)
+    else if (c > 0) right = right.insert(k, v)
+    else            value = v
+
+    var node: RBTree = this
+    if (node.left.color == Black && node.right.color == Red) node = rotateLeft(node)
+    if (node.left.color == Red && node.left.left.color == Red) node = rotateRight(node)
+    if (node.left.color == Red && node.right.color == Red) flipColors(node)
+    node
+  }
+
+  def height = left.height + color
 }
 
 class RBTreeMap extends RBTreeMapI {
